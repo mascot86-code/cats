c162d08d1f2cd919aa7aab90c21f000e5dc37ebb
mascot86 <mascot86@mail.ru>
2025-07-28 14:48:41 +0300

    Create project

---
+++ main.js
@@ -0,0 +1,134 @@
+
+// const cats = [
+//   {
+//     "id": "18t",
+//     "url": "https://cdn2.thecatapi.com/images/18t.gif",
+//     "width": 307,
+//     "height": 139,
+//     "isFavorite": true
+//   },
+//   {
+//     "id": "5d6",
+//     "url": "https://cdn2.thecatapi.com/images/5d6.jpg",
+//     "width": 500,
+//     "height": 368,
+//     "isFavorite": false
+//   },
+//   {
+//     "id": "8ro",
+//     "url": "https://cdn2.thecatapi.com/images/8ro.jpg",
+//     "width": 600,
+//     "height": 399,
+//     "isFavorite": false
+//   },
+//   {
+//     "id": "9qk",
+//     "url": "https://cdn2.thecatapi.com/images/9qk.jpg",
+//     "width": 560,
+//     "height": 746,
+//     "isFavorite": false
+//   },
+//   {
+//     "id": "bjt",
+//     "url": "https://cdn2.thecatapi.com/images/bjt.jpg",
+//     "width": 628,
+//     "height": 471,
+//     "isFavorite": false
+//   },
+//   {
+//     "id": "cdm",
+//     "url": "https://cdn2.thecatapi.com/images/cdm.jpg",
+//     "width": 671,
+//     "height": 1000,
+//     "isFavorite": false
+//   },
+//   {
+//     "id": "dbk",
+//     "url": "https://cdn2.thecatapi.com/images/dbk.jpg",
+//     "width": 500,
+//     "height": 332,
+//     "isFavorite": false
+//   },
+//   {
+//     "id": "df1",
+//     "url": "https://cdn2.thecatapi.com/images/df1.jpg",
+//     "width": 2896,
+//     "height": 1944,
+//     "isFavorite": false
+//   },
+//   {
+//     "id": "MTYyMTYwOQ",
+//     "url": "https://cdn2.thecatapi.com/images/MTYyMTYwOQ.jpg",
+//     "width": 960,
+//     "height": 574,
+//     "isFavorite": false
+//   },
+//   {
+//     "id": "MTgxODI2MA",
+//     "url": "https://cdn2.thecatapi.com/images/MTgxODI2MA.jpg",
+//     "width": 600,
+//     "height": 800,
+//     "isFavorite": false
+//   }
+// ]
+
+const catsList = document.querySelector('#cats-list')
+const cats = getState()
+const favoritesText = document.querySelector('#favorites')
+const favoritesContent = document.querySelector('.favorites-inner')
+
+catsList.addEventListener('click', changeFavoriteStatus)
+
+function render() {
+  catsList.innerHTML = cats.map(toCat).join('')
+  renderFavorites()
+}
+
+function toCat(cat) {
+  return `
+  
+  <img class="cat-img ${cat.isFavorite ? ' favorite' : ''}" src="${cat.url}" data-id=${cat.id}>
+  
+  `
+}
+
+
+function changeFavoriteStatus(event) {
+  if (event.target.classList.contains('cat-img')) {
+    const catId = event.target.dataset.id
+    const cat = cats.find(cat => cat.id === catId)
+    cat.isFavorite = !cat.isFavorite
+    setState()
+    render()
+  }
+}
+
+function getFavorites(cats) {
+  return cats.filter(cat => cat.isFavorite)
+}
+
+function renderFavorites() {
+  const favorites = getFavorites(cats)
+  if (favorites.length == 0) {
+    favoritesText.textContent = 'No favorites'
+    favoritesContent.innerHTML = ''
+    setState()
+  } else {
+    favoritesText.textContent = `Favorites (${favorites.length})`
+    favoritesContent.innerHTML = favorites.map(toCat).join('')
+  } 
+}
+
+
+function setState() {
+  localStorage.setItem('cats', JSON.stringify(cats))
+}
+
+function getState() {
+  const row = localStorage.getItem('cats')
+  return row ? JSON.parse(row) : []
+}
+
+render()
+
+
